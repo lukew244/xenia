f6430a82a5aac22baace0e4647df2b6a6d0c8e86
lukew244 <luke.webster86@gmail.com>
2016-08-10 11:12:05 +0100

    Testing database/bcrypt interaction

--- models/users.js
+++ models/users.js
@@ -3,7 +3,6 @@ var thinky = require(__dirname + '/../util/thinky.js');
 var r = thinky.r;
 var type = thinky.type;
 
-
 var User = thinky.createModel('User', {
   id: type.string(),
   name: type.string(),
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var User = require(__dirname + '/../models/users.js');
-
+var bcrypt = require('bcryptjs');
 
 
 /* GET users listing. */
 router.get('/new', function(req, res, next) {
   res.render('users/new', { title: 'Express' });
+
+  var currentUser = User.filter({
+    email: 'l@email.com'}).run().then(function(user) {
+      console.log("we're in the variable");
+      var hash = user[0].password;
+      console.log(hash);
+      var teststring = "test";
+      bcrypt.compare(teststring, hash, function(err, res) {
+          console.log(res);
+
+      });
+    });
+
+
+
+
 });
 
+
 router.post('/', function(req, res, next) {
-  var user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-  console.log(user);
-  user.saveAll();
-  res.redirect('/');
+  var encPassword = genPassword(req.body.password, newUser);
+
+  function newUser(hashedPassword) {
+
+    var user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    user.saveAll();
+  }
+
+
+  // res.redirect('/');
 });
+
+
+function genPassword(password, newUser) {
+  bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+          newUser(hash);
+      });
+  });
+}
+
+
+
 
 module.exports = router;
